004dd2d22650986c2300b048d75eb6eb02e9d6fa
Matt Stevenson <mattoc@mattoc.com>
2014-02-19 00:42:29 +1100

    Initial commit

---
+++ html_pygments.py
@@ -0,0 +1,56 @@
+from jinja2 import nodes
+from jinja2.ext import Extension
+from pygments.formatters import HtmlFormatter
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name, TextLexer
+import six
+
+import pelican.settings as pys
+
+
+class CodeHighlight(Extension):
+    tags = set(['syntax'])
+
+    def parse(self, parser):
+        lineno = parser.stream.next().lineno
+
+        # get language
+        try:
+            args = [parser.parse_expression()]
+        except:
+            args = ['string', 'text']  # don't highlight syntax, just format it
+
+        body = parser.parse_statements(['name:endsyntax'], drop_needle=True)
+
+        return nodes.CallBlock(self.call_method('_pygmentize', args),
+                               [], [], body).set_lineno(lineno)
+
+    def _pygmentize(self, name, caller):
+        self.options = {}
+
+        lexer = get_lexer_by_name(name)
+        content = caller()
+
+        # Fetch the defaults
+        if pys.PYGMENTS_RST_OPTIONS is not None:
+            for k, v in six.iteritems(pys.PYGMENTS_RST_OPTIONS):
+                # Locally set options overrides the defaults
+                if k not in self.options:
+                    self.options[k] = v
+
+        if ('linenos' in self.options and
+                self.options['linenos'] not in ('table', 'inline')):
+            if self.options['linenos'] == 'none':
+                self.options.pop('linenos')
+            else:
+                self.options['linenos'] = 'table'
+
+        for flag in ('nowrap', 'nobackground', 'anchorlinenos'):
+            if flag in self.options:
+                self.options[flag] = True
+
+        # noclasses should already default to False, but just in case...
+        formatter = HtmlFormatter(noclasses=False, **self.options)
+        parsed = highlight(content, lexer, formatter)
+
+        return parsed

60e64b7448741c18ef4e78888714906a3b75e325
Matt Stevenson <mattoc@mattoc.com>
2014-02-19 16:24:49 +1100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Plumage',
+      version='0.1',
+      description='Pygments highlighting for Pelican Jinja2'
+                  'HTML articles',
+      author='Matt Stevenson',
+      author_email='mattoc@mattoc.com',
+      url='https://github.com/mattoc/plumage',
+      py_modules=['plumage'],
+     )
